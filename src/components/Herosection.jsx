e447e874c8dbb1b325c8bc3dc412824b7fe322f3
John-Kimani <kimwaweru100@gmail.com>
2023-04-24 14:19:47 +0300

    feat initial homepage UI

---
+++ src/components/Herosection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Herosection() {
+  return (
+    <section>
+        <h3>Building a platform for DIY enthusiast to share commercial tools</h3>
+    </section>
+  )
+}
+
+export default Herosection
